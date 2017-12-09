b57c9f0b6fe72f72126cff1c72f2792e9d232cfe
sonalparab <sparab@stuy.edu>
2017-12-09 18:29:12 -0500

    mouseover

--- intro.js
+++ intro.js
@@ -1,12 +1,41 @@
 var addElement = function(e) {
     var list = document.getElementById('thelist');
-    console.log(list);
-    var element = document.createElement("LI");
-    var textElement = document.createTextNode('stuff');
-    element.appendChild(textElement);
-				    
+    //console.log(list);
+    
+    var element = document.createElement('li');
+    element.innerHTML = 'Stuff';
+
+    //mouseover
+    element.addEventListener('mouseover',changeHeading);
+    element.addEventListener('mouseout',changeBack);
+    
     list.appendChild(element);
+}
+
+var addMouseover = function(){
+    var list = document.getElementsByTagName('li');
+    console.log(list);
+    console.log(list[0]);
+    var len = list.length;
+    var i = 0;
+    for (;i < len; i++){
+	list[i].addEventListener('mouseover',changeHeading);
+	list[i].addEventListener('mouseout',changeBack);
+    }
+}
+
+var changeHeading = function(e) {
+    var headings = document.getElementsByTagName('h1');
+    var h = headings[0];   
+    h.innerHTML = e.srcElement.innerText;
+}
+
+var changeBack = function(e){
+    var list = document.getElementsByTagName('h1');
+    list[0].innerHTML = "Hello World!"
 }
 
 var b = document.getElementById('b');
 b.addEventListener('click', addElement);
+
+addMouseover();
